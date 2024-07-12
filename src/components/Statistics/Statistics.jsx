22289864631ede9d621867e29e7d89bb3dccaf6d
deioh <143625578+deioh@users.noreply.github.com>
2024-07-12 22:04:11 +0800

    update

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,7 +1,8 @@
 //import React from 'react'
+import data from '../../components/data/data.json';
 import s from './Statistics.module.css';
 
-export const Statistics = () => {
+export const Statistics = ({ label, percentage }) => {
   return (
     <section className={s.statistics}>
       <h2 className="title">Upload stats</h2>
