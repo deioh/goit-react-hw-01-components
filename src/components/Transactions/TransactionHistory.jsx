3fc6eac998a28cbe9ab5556529a65c899b5405ed
deioh <gamekuma@gmail.com>
2024-07-13 14:54:05 +0800

    update

--- src/components/Transactions/TransactionHistory.jsx
+++ src/components/Transactions/TransactionHistory.jsx
@@ -1,26 +1,25 @@
 //import React from 'react'
-
+import s from './Transactions.module.css'
 export const TransactionHistory = ({items}) => {
   return (
-    <table className="transaction-history">
-    <thead>
-      <tr>
+    <table className={s.TransactionHistory}>
+    <thead className={s.thead}>
+      <tr className={s.trhead}>
         <th>Type</th>
         <th>Amount</th>
         <th>Currency</th>
       </tr>
     </thead>
   
-    <tbody>
+    <tbody className={s.tbody}>
     {items.map(({ id, type, amount ,currency }) => (
-      <tr>
-        <td>{type}</td>
-        <td>{amount}</td>
-        <td>{currency}</td>
+      <tr className={s.trbody}>
+        <td className={s.tdbody}>{type}</td>
+        <td className={s.tdbody}>{amount}</td>
+        <td className={s.tdbody}>{currency}</td>
       </tr>
     ))}
     </tbody>
   </table>
 
-  
 )}
